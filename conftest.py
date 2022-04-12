b29c4305d36d399e61b5665f9ecfba1c3466ba9d
Rymarava Natalia <rymareva.nat@gmail.com>
2022-04-12 22:54:45 +0300

    implemented reading from xls file

--- conftest.py
+++ conftest.py
@@ -1,6 +1,15 @@
+import importlib
+import os
+import pandas as pd
+
 import pytest
 
 from fixture.application import Application
+
+
+fixture = None
+target = None
+
 
 
 @pytest.fixture(scope='session')
@@ -8,3 +17,22 @@ def app(request):
     fixture = Application("C:\\Users\\rymar\\PycharmProjects\\rymarava_b30_python\\AddressBookFree\\AddressBook.exe")
     request.addfinalizer(fixture.destroy)
     return fixture
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[3:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("xl_"):
+            testdata = load_from_xl(fixture[3:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).testdata
+
+
+def load_from_xl(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data\\%s.xls" % file), encoding='latin-1') as f:
+        return f.read()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,6 +1,7 @@
-def test_add_group(app):
+def test_add_group(app, xl_groups):
+    group = xl_groups
     old_list = app.group.get_group_list()
-    app.group.add_new_group("my group")
+    app.group.add_new_group(group)
     new_list = app.group.get_group_list()
-    old_list.append("my group")
+    old_list.append(group)
     assert sorted(old_list) == sorted(new_list)
